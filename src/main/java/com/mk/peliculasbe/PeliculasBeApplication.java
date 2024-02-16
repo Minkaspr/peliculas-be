8d5b6f4e34a8743e65ae76763dd14913dbffae6c
Dario <106852390+Minkaspr@users.noreply.github.com>
2024-02-16 11:38:03 -0500

    proyecto inicial

---
+++ src/main/java/com/mk/peliculasbe/PeliculasBeApplication.java
@@ -0,0 +1,13 @@
+package com.mk.peliculasbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mk/peliculasbe/PeliculasBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.peliculasbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
